db0d58d1f51751f433f9996c2a188e813ea778c6
Syedamaan08 <syedamaanraza2008@gmail.com>
2026-09-17 10:26:19 +0530

    Update students-data.js nandini

--- students/students-data.js
+++ students/students-data.js
@@ -65,7 +65,7 @@ var STUDENTS_DATA = [
   { slug: "rishu-raj", name: "Rishu Raj", initials: "S14", photo: "students/photos/rishu-raj.jpg", batch: "2026", batchLabel: "Batch 2026 – 2030, First Year", rank: 14, hasPage: true},
   { slug: "Hardik-Tiwari", name: "Hardik Tiwari", initials: "S15", photo: "students/photos/Hardik-Tiwari.jpeg", batch: "2026", batchLabel: "Batch 2026 – 2030, First Year", rank: 15, hasPage: true},
   { slug: "Abhinandan-kumar", name: "Abhinandan Kumar", initials: "S16", photo: "students/photos/Abhinandan-kumar.jpg", batch: "2026", batchLabel: "Batch 2026 – 2030, First Year", rank: 16, hasPage: true},
-  { slug: "Nandini-Singh", name: "Nandini Singh Rathore", initials: "S17", photo: "students/photos/Nandini-singh-rathore.jpeg", batch: "2026", batchLabel: "Batch 2026 – 2030, First Year", rank: 17, hasPage: true},
+  { slug: "nandini_singh_rathor", name: "Nandini Singh Rathore", initials: "S17", photo: "students/photos/Nandini-singh-rathore.jpeg", batch: "2026", batchLabel: "Batch 2026 – 2030, First Year", rank: 17, hasPage: true},
   { slug: "utsav", name: "Utsav", initials: "S18", photo: "students/photos/Utsav.png", batch: "2026", batchLabel: "Batch 2026 – 2030, First Year", rank: 18, hasPage: true},
   { slug: "Vishal", name: "Vishal", initials: "S19", photo: "students/photos/vishal.jpeg", batch: "2026", batchLabel: "Batch 2026 – 2030, First Year", rank: 19, hasPage: true},
   { slug: "Aankhein", name: "Aankhein", initials: "S20", photo: "students/photos/aankhein.jpeg", batch: "2026", batchLabel: "Batch 2026 – 2030, First Year", rank: 20, hasPage: true}
