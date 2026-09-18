524942efd073c75900f6f1f98e9f1744d6a69381
Syedamaan08 <syedamaanraza2008@gmail.com>
2026-09-19 00:04:38 +0530

    Update students-data.js

--- students/students-data.js
+++ students/students-data.js
@@ -87,5 +87,6 @@ var STUDENTS_DATA = [
   { slug: "Anand-kumar", name: "Anand Kumar ", initials: "S34", photo: "students/photos/Anand-kumar.png", batch: "2026", batchLabel: "Batch 2026 – 2030, First Year", rank: 36, hasPage: true},
   { slug: "vibhor-jaiswal", name: "Vibhor Jaiswal ", initials: "S34", photo: "students/photos/vibhor-jaiswal.png", batch: "2026", batchLabel: "Batch 2026 – 2030, First Year", rank: 37, hasPage: true},
   { slug: "HARSH-SOLANKI", name: "Harsh Solanki ", initials: "S35", photo: "students/photos/Harsh-solanki.jpeg", batch: "2026", batchLabel: "Batch 2026 – 2030, First Year", rank: 38, hasPage: true},
-  { slug: "Bhavya-A Agarwal", name: "Bhavya A Agarwal", initials: "S36", photo: "students/photos/", batch: "2026", batchLabel: "Batch 2026 – 2030, First Year", rank: 39, hasPage: true}
+  { slug: "Bhavya-A Agarwal", name: "Bhavya A Agarwal", initials: "S36", photo: "students/photos/", batch: "2026", batchLabel: "Batch 2026 – 2030, First Year", rank: 39, hasPage: true},
+  { slug: "Gatha-Jain", name: "GATHA JAIN", initials: "S37", photo: "students/photos/gatha-jain.jpeg", batch: "2026", batchLabel: "Batch 2026 – 2030, First Year", rank: 40, hasPage: true}
    ];
